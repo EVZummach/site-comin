4d5b52c898d1e12cea10690c15b06219d22e7035
Eduardo Zummach <eduardo.zummach@hotmail.com>
2023-03-01 19:58:34 -0300

    refined the code

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http');
+var countDownDate = new Date("Mar 01, 2024 07:12:00").getTime();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+function calculateRemainingTime() {
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+      
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + "d"
+  document.getElementById("hours").innerHTML = hours + "h"
+  document.getElementById("minutes").innerHTML = minutes + "m"
+  document.getElementById("seconds").innerHTML = seconds + "s"
+}
+
+
+console.log("Opa")
+
+var myfunc = setInterval(function() {
+  calculateRemainingTime()  
+}, 1000)
